c426a9ff72f70fef522de8511c3ef847397d5c97
Pietro Panizza <pietropanizza88@gmail.com>
2019-12-09 23:46:09 +0100

    Add ADC in package init file.

--- test/test_ADC.py
+++ test/test_ADC.py
@@ -15,7 +15,7 @@
 # along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
 import unittest
-from loraberry.ADC import ADC
+from loraberry import ADC
 
 class Test(unittest.TestCase):
 
